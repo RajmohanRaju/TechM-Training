e0b00e3e3e11615b1a569f71abdd6f7782c8742c
RajmohanRaju <rajmohanraju2003@gmail.com>
2025-03-13 09:10:23 +0530

    Delete src directory

--- src/app/components/navbar/navbar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-navbar', // This must match what you use in app.component.html
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
-})
-export class NavbarComponent { }
